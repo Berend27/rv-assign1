cc1656a7dd3363761f629d2dc9ac503d39d503f8
Dexter Berend <dexterjberend@gmail.com>
2026-04-03 21:44:49 +0200

    task 1 done

---
+++ build/CMakeFiles/assign1_task1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assign1_task1.dir/code/assign1_task1.cpp.o"
+  "CMakeFiles/assign1_task1.dir/code/assign1_task1.cpp.o.d"
+  "assign1_task1"
+  "assign1_task1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assign1_task1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
